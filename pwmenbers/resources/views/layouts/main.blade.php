20f1372fb029013267b7552eb9147b7d4dad5fbc
runa <murayama@suncreer.co.jp>
2021-07-01 17:03:54 +0900

    update

---
+++ pwmenbers/resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <header>
+            <img src="ヘッダー新_mint.png">
+        </header>
+        <section>
+            <main>
+                @yield('content')
+            </main>
+        </section>
+        <footer>
+            <img src="フッタ_mint.png">
+        </footer>
+    </body>
+</html>
