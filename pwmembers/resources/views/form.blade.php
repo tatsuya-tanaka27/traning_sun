b7d6758d2e8fd90eb47f9b27aa5d2538b5441285
marco <marco@suncreer.co.jp>
2021-07-14 10:51:36 +0900

    marco 07/14

--- pwmembers/resources/views/form.blade.php
+++ pwmembers/resources/views/form.blade.php
@@ -6,7 +6,10 @@
     </h1>
 <div style="margin:50px;">
 
+  <p>{{ Auth::user()->name }} 様</p>
+
   <div class="form-group row">
+    <div class="col-sm-1"></div>
     <label for="select1a" class="col-sm-1 col-form-label">お問い合わせ種別</label>
     <div class=" col-sm-8">
     <select id="select1a" class="form-control">
@@ -16,23 +19,31 @@
       <option>その他</option>
     </select>
     </div>
+
+    <div class="col-sm-2"></div>
   </div>
 
   <div class="form-group row">
+    <div class="col-sm-1"></div>
     <label for="inputPassword" class="col-sm-1 col-form-label">件名</label>
     <div class="col-sm-8">
       <input class="form-control" id="">
     </div>
+    <div class="col-sm-2"></div>
   </div>
 
   <div class="form-group row">
+    <div class="col-sm-1"></div>
     <label for="text1" class="col-sm-1 col-form-label">お問い合わせ内容</label>
     <div class="col-sm-8">
-    <input type="text" id="text1" class="form-control" style="height:200px">
+    <textarea class="form-control" rows="3"></textarea>
     </div>
+    <div class="col-sm-2"></div>
 </div>
-
-
 </form>
+
+<div class="form-group row">
+<div class="col-sm-2"></div>
+<button type="button" class="btn btn-info">送　信</button>
 </div>
 @endsection
